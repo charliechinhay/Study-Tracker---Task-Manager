18073d61c7769a55a8353608235eaa849d7aea7f
charlie.chinchay <hairolas123@hotmail.com>
2026-03-29 17:47:55 +0100

    add port, server.js, cors, routes

--- server/server.js
+++ server/server.js
@@ -1,2 +1,20 @@
 import connectDB from "./db";
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+import taskRoutes from "./routes/task.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 connectDB();
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
