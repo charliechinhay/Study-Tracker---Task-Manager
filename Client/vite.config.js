b933dc9dc5f947da4d80660afac8399facd59f64
charlie chinchay <hairolas123@hotmail.com>
2026-05-14 23:07:46 +0200

    clean deploy

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsx: "automatic",
-  },
   test: {
     globals: true,
     environment: "jsdom",
