04beec618ae594f5c3886bf2cabbcb55295f4200
charlie chinchay <hairolas123@hotmail.com>
2026-05-14 23:48:46 +0200

    debug-trydemo

--- server/app.js
+++ server/app.js
@@ -8,7 +8,24 @@ import passport from "./config/passport.js";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:5173",
+  "http://localhost:5174",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(
   session({
